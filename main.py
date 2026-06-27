934cd92f142297ea3631bb4c39aa76478459d8a7
beerkamalbhagal <bbhagal06@gmail.com>
2026-06-26 21:41:31 -0400

    added pass and interceptions

--- main.py
+++ main.py
@@ -1,12 +1,13 @@
 from utils.video_utils import read_video, save_video
 from trackers import PlayerTracker, BallTracker
-from drawers import PlayerTracksDrawer, BallTracksDrawer, ball_tracks_drawer
+from drawers import PlayerTracksDrawer, BallTracksDrawer, TeamBallControlDrawer, PassInterceptionDrawer
 from team_assigner import TeamAssigner
 from ball_acquisition import BallAcquisitionDetector
+from pass_and_interception_detector import PassAndInterceptionDetector
 
 def main():
     # Read video   
-    video_frames = read_video("input_videos/video_1.mp4")
+    video_frames = read_video("input_videos/video_2.mp4")
 
     # Initialize trackers
     player_tracker = PlayerTracker("models/player_detector.pt")
@@ -34,14 +35,25 @@ def main():
     ball_acquisition_detector = BallAcquisitionDetector()
     ball_acquisition = ball_acquisition_detector.detect_ball_possession(player_tracks, ball_tracks)
 
-    # Draw output
+    # Passes and Interceptions
+    pass_and_interception_detector = PassAndInterceptionDetector()
+    passes = pass_and_interception_detector.detect_passes(ball_acquisition, player_assignment)
+    interceptions = pass_and_interception_detector.detect_interceptions(ball_acquisition, player_assignment)
+
+      # Draw output
     # Initialize drawers
     player_tracks_drawer = PlayerTracksDrawer()
     ball_tracks_drawer = BallTracksDrawer()
+    team_ball_control_drawer = TeamBallControlDrawer()
+    pass_interception_drawer = PassInterceptionDrawer()
 
     # Draw object tracks
     output_video_frames = player_tracks_drawer.draw(video_frames, player_tracks, player_assignment, ball_acquisition)
     output_video_frames = ball_tracks_drawer.draw(output_video_frames, ball_tracks)
+    output_video_frames = team_ball_control_drawer.draw(output_video_frames, player_assignment, ball_acquisition)
+    output_video_frames = pass_interception_drawer.draw(output_video_frames, passes, interceptions)
+    output_video_frames = pass_interception_drawer.draw(output_video_frames, passes, interceptions)
+
     # Save video   
     save_video(output_video_frames, "output_videos/output_video.avi") 
 
